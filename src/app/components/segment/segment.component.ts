a873817a744da130e58e7a61ba5e18de5bcd36e6
Gabie <jacadel1998@hotmail.com>
2020-08-13 11:57:53 +0200

    Angular update and Latex segment support

--- src/app/components/segment/segment.component.ts
+++ src/app/components/segment/segment.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Segment } from 'src/app/model/segment';
+import { KatexOptions } from 'ng-katex';
 
 @Component({
 	selector: 'segment',
@@ -9,6 +10,11 @@ import { Segment } from 'src/app/model/segment';
 export class SegmentComponent implements OnInit {
 
 	@Input() segment: Segment;
+
+	options: KatexOptions = {
+		displayMode: true,
+		
+	};
 
 	constructor() { }
 
