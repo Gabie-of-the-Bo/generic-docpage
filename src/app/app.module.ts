432d75e26c7d2debe16d838939a6f85f0a0c9ea9
Gabie <jacadel1998@hotmail.com>
2020-08-14 18:35:38 +0200

    Ace Editor basic integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { KatexModule } from 'ng-katex';
+import { AceEditorModule } from 'ng2-ace-editor';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +21,8 @@ import { SegmentComponent } from './components/segment/segment.component';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    KatexModule
+	KatexModule,
+	AceEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/segment/segment.component.ts
+++ src/app/components/segment/segment.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Segment } from 'src/app/model/segment';
 import { KatexOptions } from 'ng-katex';
 
+/*
+	Import your Ace Editor modes here
+*/
+import 'brace/mode/python';
+
 @Component({
 	selector: 'segment',
 	templateUrl: './segment.component.html',
