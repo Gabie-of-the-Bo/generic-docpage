d34b873096e2299f71b84766ce68a3ef5023c677
Gabie <jacadel1998@hotmail.com>
2020-08-18 18:51:10 +0200

    Segment model update

--- src/app/model/segment.ts
+++ src/app/model/segment.ts
@@ -2,12 +2,14 @@ export class Segment{
     type: string;
     content: string;
     language: string;
+    inline_css: string;
     page: number;
 
-    public constructor(type: string, content: string, language?: string, page?: number){
+    public constructor(type: string, content: string, language?: string, inline_css?: string, page?: number){
         this.type = type;
         this.content = content;
         this.language = language;
+        this.inline_css = inline_css;
         this.page = page? page : 1;
     }
 }
